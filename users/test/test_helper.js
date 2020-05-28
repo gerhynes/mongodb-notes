d3f74d43d2891121d4bb5f7d817e6c7ef47efb0d
Gerard Hynes <gkhynes@gmail.com>
2020-05-28 21:58:38 +0100

    Clear list of users before running test

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -7,8 +7,16 @@ mongoose.connect("mongodb://localhost/users_test", {
 
 mongoose.connection
   .once("open", () => {
-    console.log("Connection is working, ya big eejit");
+    console.log("Connection is working");
   })
   .on("error", (error) => {
     console.warn("Warning", error);
   });
+
+// Before each test clear out list of users
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run next test
+    done();
+  });
+});
