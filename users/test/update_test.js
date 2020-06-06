981e527ded0d3df9bfdcd03861e9e848b1811c51
Gerard Hynes <gkhynes@gmail.com>
2020-06-06 12:59:52 +0100

    Add test for incrementing postCount

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -44,7 +44,12 @@ describe("Updating records", () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Joseph" }), done);
   });
 
-  it("a user can have their postCount incremented by 1", () => {
-    
+  it("a user can have their postCount incremented by 1", (done) => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 1 } }).then(() => {
+      User.findOne({ name: "Joe" }).then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
+    });
   });
 });
