d2803a3fd216e028efecfdf85de74db22b252766
Gerard Hynes <gkhynes@gmail.com>
2020-06-14 13:01:39 +0100

    Add api route and test

--- muber/app.js
+++ muber/app.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// Watch for incoming GET requests to http://localhost:3050/api
+app.get("/api", (req, res) => {
+  res.send({ hi: "there" });
+});
+
 module.exports = app;
