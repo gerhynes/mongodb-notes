4c14d0b3dd8db1801bad867f2e4b3ea2c2dade27
Gerard Hynes <gkhynes@gmail.com>
2020-06-14 15:09:30 +0100

    Refactor out routes and controllers

--- muber/app.js
+++ muber/app.js
@@ -1,10 +1,7 @@
 const express = require("express");
-
+const routes = require("./routes/routes");
 const app = express();
 
-// Watch for incoming GET requests to http://localhost:3050/api
-app.get("/api", (req, res) => {
-  res.send({ hi: "there" });
-});
+routes(app);
 
 module.exports = app;
