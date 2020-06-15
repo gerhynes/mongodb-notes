bf9d4b168d21e336411a701965435f67c76584f9
Gerard Hynes <gkhynes@gmail.com>
2020-06-15 21:23:54 +0100

    Add and test route to create drivers

--- muber/routes/routes.js
+++ muber/routes/routes.js
@@ -3,4 +3,6 @@ const DriversController = require("../controllers/drivers_controller");
 module.exports = (app) => {
   // Watch for incoming GET requests to http://localhost:3050/api
   app.get("/api", DriversController.greeting);
+
+  app.post("/api/drivers", DriversController.create);
 };
