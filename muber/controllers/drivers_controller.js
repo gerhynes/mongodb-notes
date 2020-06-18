60090b032e3ca909cbcd4e93420d9b13257eaae0
Gerard Hynes <gkhynes@gmail.com>
2020-06-18 19:52:48 +0100

    Add driver index handler and test

--- muber/controllers/drivers_controller.js
+++ muber/controllers/drivers_controller.js
@@ -5,6 +5,26 @@ module.exports = {
     res.send({ hi: "there" });
   },
 
+  index(req, res, next) {
+    const { lng, lat } = req.query;
+
+    Driver.aggregate([
+      {
+        $geoNear: {
+          near: {
+            type: "Point",
+            coordinates: [parseFloat(lng), parseFloat(lat)]
+          },
+          maxDistance: 200000,
+          spherical: true,
+          distanceField: "dist.calculated"
+        }
+      }
+    ])
+      .then((drivers) => res.send(drivers))
+      .catch(next);
+  },
+
   create(req, res, next) {
     const driverProps = req.body;
 
